2c8993baad6ff8fd43e4c0a0afdaca345fb935bf
Michael Kewming <michael@MACBOOK-PRO-4.station>
2023-02-12 22:00:40 +0000

    debugging issues with implementation. Also using notebooks for testing

--- Shreve_exercises/Binomial_asset_tree2.py
+++ Shreve_exercises/Binomial_asset_tree2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from functools import wraps
 from time import time
+from scipy.special import binom
 
 def timing(f):
     # Create timing wrapper
@@ -69,15 +70,7 @@ class binomial_tree:
         # Calculate X0
         return sum(self.disc * self.calculate_Vn(1) * np.array([1-self.p, self.p]))
 
-    # def calculate_Xn(self, n):
-    #     # Calculate wealth an maturation of european option
-    #     xn = self.X0
-    #     for i range(n):
-    #         dn = self.calculate_deltan(i)
-    #         sn = self.calculate_Sn(i)
-    #         xn = self.d0 * self.calculate_Sn(1) + (1/self.disc) * (self.V0 - self.d0*self.S0)
-    #
-    #     return self.X1
+# Let's compute some trajectories stuff: given a length n, generate a random list of -1 and 1's for downs and ups
 
 
 if __name__ == "__main__":
@@ -93,9 +86,24 @@ if __name__ == "__main__":
                 'N': 10,
                 'T': 1}
 
+    # create random model
     m = binomial_tree(params)
+
+    n = 3
+    Sn = m.calculate_Sn(n)
+    Vn = m.calculate_Vn(n)
+    probs = (1-m.p)**(np.arange(n, -1, -1)) * (m.p)**(np.arange(0, n+1, 1))
+    bs = binom(n, np.arange(n+1))
+
+    # C
+    Sbar =  sum(bs * probs * Sn) * m.disc ** n
+    Vbar = sum(bs * probs * Vn) * m.disc ** n
+
+    print("V0 = ", np.round(m.V0, 3), " and discounted average o Vn = ", np.round(Vbar, 3))
+    print("S0 = ", m.S0, " and discounted average o Vn = ", np.round(Sbar, 3))
+
+
     # print(np.mean(m.calculate_X1()))
     # print(np.mean(m.calculate_Vn(1)))
-
 
 
